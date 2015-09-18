d9a82e73f70856d4779261946863bd68480be5a5
s.savran <s.savran@itransition.com>
2015-09-18 14:25:14 +0300

    Merge branch 'master' of https://github.com/cobaltSS/strike_promo
    # Conflicts:
    #	db.sqlite3

--- t/settings.py
+++ t/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 't.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-     #   'NAME': 'promo',
-     #   'USER': 'django',
-     #   'PASSWORD': 'TTaSOAGj1r',
-     #   'HOST': 'localhost',
-     #    'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'promo',
+       'USER': 'django',
+       'PASSWORD': 'TTaSOAGj1r',
+       'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -93,9 +93,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
