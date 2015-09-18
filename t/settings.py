d3c1e5493d3ee5d51eb09622e4715c5632d814e0
s.savran <s.savran@itransition.com>
2015-09-18 14:20:03 +0300

    Merge branch 'master' of https://github.com/cobaltSS/strike_promo
    # Conflicts:
    #	db.sqlite3

--- t/settings.py
+++ t/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 't.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'promo',
-       'USER': 'django',
-       'PASSWORD': 'TTaSOAGj1r',
-       'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     #   'NAME': 'promo',
+     #   'USER': 'django',
+     #   'PASSWORD': 'TTaSOAGj1r',
+     #   'HOST': 'localhost',
+     #    'PORT': '5432',
     }
 }
 
